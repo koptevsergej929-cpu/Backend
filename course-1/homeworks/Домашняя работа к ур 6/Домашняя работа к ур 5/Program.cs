58844a56f205cbd4c86b198e1db645c25c09a7b8
Павел <koptevsergej929@gmail.com>
2025-12-03 16:52:51 +0300

    Добавил папку с первым курсом

---
+++ course-1/homeworks/Домашняя работа к ур 6/Домашняя работа к ур 5/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задание 1
+//Console.Write("Введите температуру в градусах цельсия");
+//string input = Console.ReadLine();
+//int num1 = int.Parse(input);
+
+//int num3 = 10;
+//int num4 = 32;
+//int sum = num1 * num2 / num3 + num4;
+//Console.WriteLine($"{num1} * {num2} / {num3} + {num4} = {sum}");
+
+
+//Задание 2
+//Console.Write("Введите одно любое число");
+//string input1 = Console.ReadLine();
+//int num1 = int.Parse(input1);
+
+//Console.Write("Введите одно любое число");
+//string input2 = Console.ReadLine();
+//int num2 = int.Parse(input2);
+
+//Console.Write("Введите одно любое число");
+//string input3 = Console.ReadLine();
+//int num3 = int.Parse(input3);
+
+//int num4 = 3;
+
+//int sum = (num1 + num2 + num3) / num4;
+//Console.WriteLine($"({num1} + {num2} + {num3}) / {num4} = {sum}");
+
+
+//Задание 3
+//Console.Write("Введите одно любое число");
+//string input1 = Console.ReadLine();
+//int num1 = int.Parse(input1);
+
+//Console.Write("Введите одно любое число");
+//string input2 = Console.ReadLine();
+//int num2 = int.Parse(input2);
+
+//int sum1 = num1 + num2;
+//int sum2 = num1 - num2;
+//int sum3 = num1 * num2;
+//int sum4 = num1 / num2;
+
+//Console.WriteLine($"{num1} + {num2} = {sum1}");
+//Console.WriteLine($"{num1} - {num2} = {sum2}");
+//Console.WriteLine($"{num1} * {num2} = {sum3}");
+//Console.WriteLine($"{num1} / {num2} = {sum4}");
---
+++ course-1/homeworks/Задание с 7 урока номер 5/Задание с 7 урока номер 5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Задание_с_7_урока_номер_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 5: Калькулятор оценнок 
+            Console.Write("Введите свою оценку");
+
+            string input = Console.ReadLine();
+            int a = int.Parse(input);
+
+            if (a < 3)
+            {
+                Console.WriteLine("Неудовлетворительно");
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine("Удовлетворительно");
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine("Хорошо");
+            }
+            else if (a == 5)
+            {
+                Console.WriteLine("Отлично");
+            }
+
+
+         }  
+    }
+}
