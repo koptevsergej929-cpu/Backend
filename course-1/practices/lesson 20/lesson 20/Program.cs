e87fc2de1730d7562f2b6c6e5105acf8d39f5ab5
Pasha <koptevpavel929@gmail.com>
2026-02-06 19:53:33 +0300

    Added an edit

---
+++ course-1/practices/lesson 20/lesson 20/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace lesson_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string path = "notes.txt";
+
+            //    File.WriteAllLines(path, new[]
+            //    {
+            //        "Заметка 1",
+            //        "Заметка 2",
+            //        "Заметка 3"
+            //    });
+
+            //    File.AppendAllText(path, Environment.NewLine + "Заметка 4");
+
+            //    Console.WriteLine("Содержимое файла");
+            //    Console.WriteLine(File.ReadAllText(path));
+
+            //    Console.Write("Удалить файл? (y/n): ");
+            //    if (Console.ReadLine()?.Trim().ToLowerInvariant() == "y")
+            //    {
+            //        File.Delete(path);
+            //        Console.WriteLine("Файл удалён.");
+            //    }
+            //}
+
+
+            string dir = "data";
+
+            Directory.CreateDirectory(dir);
+
+            File.WriteAllText(Path.Combine(dir, "a.txt"), "A");
+            File.WriteAllText(Path.Combine(dir, "b.txt"), "BBBB");
+            File.WriteAllText(Path.Combine(dir, "b.md"), "CCC");
+
+            foreach (var f in Directory.GetFiles(dir, "*.txt"))
+            {
+                var info = new FileInfo(f);
+                Console.WriteLine($"{info.Name} -- {info.Length}");
+            }
+        }
+    }
+}
---
+++ course-1/practices/lesson 21/lesson 21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace lesson_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string _PATH_ = "C:\\Users\\admin\\Desktop\\pazzle-portfolio\\course-1\\practices\\lesson 21\\lesson 21\\Book.cs";
+
+            var library = new Library()
+            {
+                Name = "City library",
+                Books = new List<Book>
+                {
+                new Book{Title = "book 1", Author = "Author 1", Year = 1869},
+                new Book{Title = "book 2", Author = "Author 2", Year = 1866}
+                }
+            };
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(library, options);
+
+            File.WriteAllText(_PATH_, json);
+
+            var jsonFromFile = File.ReadAllText(_PATH_);
+            var libraryFromFile = JsonSerializer.Deserialize<List<Book>>(jsonFromFile);
+
+            if (libraryFromFile != null)
+            {
+                Console.WriteLine($"Библеотека: {libraryFromFile.Name}");
+                foreach (var book in libraryFromFile.Books)
+                {
+                    Console.WriteLine($"Название: {book.Title}, Автор: {book.Author}, Год: {book.Year}");
+                }
+            }
+
+
+        }
+    }
+}
