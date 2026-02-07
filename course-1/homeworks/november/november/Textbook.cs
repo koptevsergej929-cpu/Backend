bad20819a03a528a0490491ce2d77b61fcc458b0
Pasha <koptevpavel929@gmail.com>
2026-02-07 21:03:08 +0300

    Add homework

---
+++ course-1/homeworks/november/november/Textbook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace november
+{
+    public class Textbook : Book
+    {
+        public string Subject { get; set; }
+
+        public Textbook(string title, string author, int year, int pages, string subject)
+            : base(title, author, year, pages)
+        {
+            Subject = subject;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Учебник: {Title}, Предмет: {Subject}, Автор: {Author}, Год: {Year}, Страниц: {Pages}");
+        }
+    }
+
+    public class FictionBook : Book
+    {
+        public string Genre { get; set; }
+
+        public FictionBook(string title, string author, int year, int pages, string genre)
+            : base(title, author, year, pages)
+        {
+            Genre = genre;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Художественная книга: {Title}, Жанр: {Genre}, Автор: {Author}, Год: {Year}, Страниц: {Pages}");
+        }
+
+    }
+}
