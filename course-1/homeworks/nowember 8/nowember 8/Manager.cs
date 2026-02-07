369750cf69fba88b61ea9d13ee0025ca0402d991
Pasha <koptevpavel929@gmail.com>
2026-02-07 20:45:22 +0300

    Add homework

---
+++ course-1/homeworks/nowember 8/nowember 8/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nowember_8
+{
+    public class Manager : Worker
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} - Планирует задачи");
+        }
+    }
+}
