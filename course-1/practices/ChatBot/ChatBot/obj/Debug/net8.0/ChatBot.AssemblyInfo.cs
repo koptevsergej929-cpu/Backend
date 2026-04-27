70d9643b92576520e47ad76f465712568ef35ead
Pasha <koptevpavel929@gmail.com>
2026-04-27 22:10:15 +0300

    Updated prsctices

--- course-1/practices/ChatBot/ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
+++ course-1/practices/ChatBot/ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588beb93abc92bd6bac3a25596eccec228b1df63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
